a85f9cb734fb23709d0adee739ba77acb5d596ae
Sebastian Arambulo <earambulo.sa@gmail.com>
2022-07-07 19:58:33 -0400

    Add project files.

---
+++ Veterinaria/Scripts/JS/raza.js
@@ -0,0 +1,117 @@
+﻿//Listar()
+
+//function Listar() {
+//    $.get("/Raza/ListarRazas", function (data) {
+//        crearListado(data);
+//    })
+//}
+
+
+function crearListado(data) {
+    console.log(data);
+
+    var contenido = "";
+    contenido += "<table class='table table-hover table-bordered'>";
+    contenido += "<tr>";
+    contenido += "<th>Cod. Raza</th>"
+    contenido += "<th>Descripción</th>"
+    contenido += "<th>Esta Activo?</th>"
+    contenido += "<th>Acciones</th>"
+    contenido += "</tr>";
+
+    var fila;
+    for (var i = 0; i < data.length; i++) {
+        fila = data[i];
+        contenido += "<tr>";
+        contenido += "<td>";
+        contenido += fila.RazaID;
+        contenido += "</td>";
+        contenido += "<td>";
+        contenido += fila.Descripcion;
+        contenido += "</td>";
+        contenido += "<td>";
+        contenido += fila.EstaActivo;
+        contenido += "</td>";
+        contenido += `<td>
+                        <button data-bs-toggle="modal" onclick="AbrirModal(${fila.RazaID})" data-bs-target="#exampleModal" class="btn btn-primary bi bi-pencil-square"></button>
+                        <button onclick="Eliminar(${fila.RazaID})" class="btn btn-danger bi bi-file-x"></button>
+                      </td>
+                        `
+        contenido += "</tr>";
+    }
+
+    contenido += "</table>";
+    document.getElementById("DivRaza").innerHTML = contenido;
+
+
+}
+
+function AbrirModal(id) {
+    if (id != undefined) {
+        $.get("/raza/RecuperarRaza/" + id, function (data) {
+            document.getElementById("txtRazaID").value = data[0].RazaID;
+            document.getElementById("txtDescripcion").value = data[0].Descripcion;
+            document.getElementById("cboEstaActivo").value = data[0].EstaActivo;
+
+        })
+    }
+    Limpiar();
+
+}
+
+function Limpiar() {
+    document.getElementById("txtRazaID").value = "";
+    document.getElementById("txtDescripcion").value = "";
+    document.getElementById("cboEstaActivo").value = "";
+}
+
+function Eliminar(id) {
+    //alert(id)
+    var frm = new FormData();
+    frm.append("id", id);
+    $.ajax({
+        type: "POST",
+        url: "raza/Eliminar",
+        contentType: false,
+        processData: false,
+        data: frm,
+        success: function (data) {
+            if (data == 0) {
+                alert("Se ha producido un error.")
+            }
+            else {
+                Listar();
+                alert("Registro eliminado.")
+            }
+        }
+    })
+}
+
+
+function Guardar() {
+
+    var raza_id = document.getElementById("txtRazaID").value;
+    var descripcion = document.getElementById("txtDescripcion").value;
+    var esta_activo = document.getElementById("cboEstaActivo").value;
+    var frm = new FormData();
+    frm.append("RazaID", raza_id);
+    frm.append("Descripcion", descripcion);
+    frm.append("EstaActivo", esta_activo)
+    $.ajax({
+        type: "POST",
+        url: "raza/Guardar",
+        contentType: false,
+        processData: false,
+        data: frm,
+        success: function (data) {
+            if (data == 0) {
+                alert("Se ha producido un error.")
+            }
+            else {
+                Listar();
+                alert("Se ha registrado correctamente")
+                document.getElementById("btnCerrar").click();
+            }
+        }
+    })
+}
---
+++ Veterinaria/Views/Raza/Index.vbhtml
@@ -0,0 +1,48 @@
+﻿@Code
+    ViewData("Title") = "Index"
+    Layout = "~/Views/Plantillas/Dashboard.vbhtml"
+End Code
+
+<h2>Mantenimiento de Razas</h2>
+<button onclick="AbrirModal()" type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Nueva Raza
+</button>
+
+<div id="DivRaza">
+
+</div>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <input type="text" class="form-control" id="txtRazaID">
+
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Raza</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="descripcion" class="form-label">Descripcion</label>
+                    <input type="text" class="form-control" id="txtDescripcion">
+                </div>
+
+                <div class="modal-body">
+                    <label for="esta_activo" class="form-label">Esta Activo?</label>
+                    <select class="form-select" id="cboEstaActivo">
+                        <option value="S">Si</option>
+                        <option value="N">No</option>
+                    </select>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary" onclick="Guardar()">Guardar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="~/Scripts/JS/jquery-3.6.0.min.js"></script>
+<script src="~/Scripts/JS/raza.js"></script>
